0d059b24f43c6bb7b59d1e18efecaa1c7148e8dc
Onintso <60614002+Onintso@users.noreply.github.com>
2023-04-12 20:45:28 +0200

    initialisation ejb singleton

---
+++ src/main/java/mg/itu/tpbanquerandriamiarinamanana41/ejb/CreationCompteInitial.java
@@ -0,0 +1,22 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package mg.itu.tpbanquerandriamiarinamanana41.ejb;
+
+import jakarta.annotation.PostConstruct;
+import jakarta.ejb.Singleton;
+import jakarta.ejb.Startup;
+
+/**
+ *
+ * @author randr
+ */
+@Singleton
+@Startup
+public class CreationCompteInitial {
+    @PostConstruct
+        public void init() {
+            
+        }
+}
