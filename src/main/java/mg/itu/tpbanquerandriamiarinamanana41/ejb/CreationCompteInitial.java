33dffd8cbe05b6f03f8060d2a4b07bad2059cedb
Onintso <60614002+Onintso@users.noreply.github.com>
2023-04-13 20:24:35 +0200

    creer fonction init dans creationCompteInitial et changer dans persistence.xml

--- src/main/java/mg/itu/tpbanquerandriamiarinamanana41/ejb/CreationCompteInitial.java
+++ src/main/java/mg/itu/tpbanquerandriamiarinamanana41/ejb/CreationCompteInitial.java
@@ -5,8 +5,10 @@
 package mg.itu.tpbanquerandriamiarinamanana41.ejb;
 
 import jakarta.annotation.PostConstruct;
+import jakarta.ejb.EJB;
 import jakarta.ejb.Singleton;
 import jakarta.ejb.Startup;
+import mg.itu.tpbanquerandriamiarinamanana41.entities.CompteBancaire;
 
 /**
  *
@@ -15,8 +17,21 @@ import jakarta.ejb.Startup;
 @Singleton
 @Startup
 public class CreationCompteInitial {
+    @EJB
+    GestionnaireCompte gestionnaireCompte;
+    
     @PostConstruct
         public void init() {
+            long nb = gestionnaireCompte.nbComptes();
+            if(nb==0){
+                CompteBancaire[] listeCompte = new CompteBancaire[4];
+                listeCompte[0]=new CompteBancaire("John Lennon", 150000);
+                listeCompte[1]=new CompteBancaire("Paul McCartney", 950000);
+                listeCompte[2]=new CompteBancaire("Ringo Starr", 20000);
+                listeCompte[3]=new CompteBancaire("Georges Harrisson", 100000);
+                for(int i=0; i<4; i++)
+                    gestionnaireCompte.creerCompte(listeCompte[i]);
+            }
             
         }
 }
